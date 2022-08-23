acde75c546bd1c64724684170a0f6e8e3f2eeeee
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-23 08:13:38 -0300

    #4-release: implementando modelo aluno e testando insert no django admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from escola.models import Aluno
+
+
+class Alunos(admin.ModelAdmin):
+    list_display = ("id", "nome", "rg", "cpf", "dt_nasc")
+    list_display_links = ("id", "nome")
+    search_fields = (
+        "nome",
+        "cpf",
+    )
+    list_per_page: 10
+
+
+admin.site.register(Aluno, Alunos)
