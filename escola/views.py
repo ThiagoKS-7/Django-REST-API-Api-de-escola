ffa763e08b7ca874430639a854acb81828a47951
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2022-08-24 07:46:17 -0300

    #6-release: ajustando lógica do endpoint de escola e adicionando as opções do curso

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import AlunoSerializer
-from escola.models import Aluno
+from .serializers import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework import permissions
 from drf_yasg.utils import swagger_auto_schema
@@ -35,7 +35,32 @@ class AlunoDetail(APIView):
         return Response(
             {
                 "errors": serializer.errors,
-                "message": "Houverar erros de validação - formato de email inválido",
+                "message": "Houveram erros de validação - formato de email inválido",
+            },
+            status=HTTP_400_BAD_REQUEST,
+        )
+
+
+class CursoList(APIView):
+    def get(self, request, format=None):
+        Cursos = Curso.objects.all()
+        serializer = CursoSerializer(Cursos, many=True)  # o many é True qnd for lista
+        return Response(serializer.data, status=HTTP_200_OK)
+
+
+class CursoDetail(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    @swagger_auto_schema(request_body=CursoSerializer)
+    def post(self, request, format=None):
+        serializer = CursoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(
+            {
+                "errors": serializer.errors,
+                "message": "Houveram erros de validação - formato de email inválido",
             },
             status=HTTP_400_BAD_REQUEST,
         )
